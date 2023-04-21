444b0e9d0bc5aea39598872448ecbb2a248e13e0
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-04-20 23:41:42 -0700

    :lipstick: Render Imgs
    Add a new user interface that allows users to like the images. It includes buttons with emoji and displays how many likes for each image.

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import Emoji from './emoji';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helloCounter: 0,
+    };
+  }
+  incremenLikes = () => {
+    this.setState({
+      helloCounter: this.state.helloCounter + 1,
+    });
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl} alt={this.title} height={250} />
-        <p>{this.props.description}</p>
-      </React.Fragment>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.imgUrl} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>{this.state.helloCounter} Favorites</Card.Text>
+          <Button variant="primary" onClick={this.incremenLikes}>
+            Likes <Emoji symbol="❤️️" label="heart" />
+          </Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
