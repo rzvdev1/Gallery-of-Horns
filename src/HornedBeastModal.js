0729a3af477dc7f174faf47e277cabebfe70668d
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-04-23 21:54:03 -0700

    :construction: Render React Modal

--- src/HornedBeastModal.js
+++ src/HornedBeastModal.js
@@ -3,19 +3,28 @@ import { Button, Modal } from 'react-bootstrap';
 
 class HornedBeastModal extends React.Component {
   render() {
+    console.log(this.props.beasts);
     return (
       <Modal
         show={this.props.showModal}
         onHide={this.props.hideHornedBeastModal}
       >
         <Modal.Header closeButton>
-          <Modal.Title>{this.props.beasts}</Modal.Title>
+          <Modal.Title>{this.props.beasts.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <img src={this.props.image_url} alt={this.props.title}></img>
+          <img
+            src={this.props.beasts.image_url}
+            alt={this.props.beasts.title}
+          ></img>
+          <p>{this.props.beasts.description}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={this.props.hideHornedBeastModal}>
+          <Button
+            variant="primary"
+            className="btn btn-primary btn-lg"
+            onClick={this.props.hideHornedBeastModal}
+          >
             Close
           </Button>
         </Modal.Footer>
