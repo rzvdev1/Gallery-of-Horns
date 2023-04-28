f78726c3d94ff1df078fac4575b3f50d4e1107e3
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-04-27 19:56:48 -0700

    :sparkles: Added Form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import HornedBeastModal from './HornedBeastModal';
+import data from './data.json';
+import { Form } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +12,7 @@ class App extends React.Component {
     this.state = {
       beasts: {},
       showModal: false,
+      data: data,
     };
   }
 
@@ -31,13 +34,44 @@ class App extends React.Component {
     });
   };
 
+  changeHorn = (e) => {
+    e.preventDefault();
+    let filterHorn;
+
+    if (e.target.value === 'one') {
+      filterHorn = data.filter((horn) => parseInt(horn.horns) === 1);
+    } else if (e.target.value === 'two') {
+      filterHorn = data.filter((horn) => parseInt(horn.horns) === 2);
+    } else if (e.target.value === 'three') {
+      filterHorn = data.filter((horn) => parseInt(horn.horns) === 3);
+    } else if (e.target.value === 'onehundred') {
+      filterHorn = data.filter((horn) => parseInt(horn.horns) === 100);
+    } else {
+      filterHorn = data;
+    }
+    this.setState({ data: filterHorn });
+  };
+
   render() {
     return (
       <>
         <Header></Header>
+        <Form>
+          <Form.Group>
+            <Form.Label>Choose Horn</Form.Label>
+            <Form.Select onChange={this.changeHorn}>
+              <option value="">Open this select menu</option>
+              <option value="one">One Horn</option>
+              <option value="two">Two Horn</option>
+              <option value="three">Three Horn</option>
+              <option value="onehundred">One Hundred Horn</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
         <Main
           showHornedBeastModal={this.showHornedBeastModal}
           selectBeasts={this.selectBeasts}
+          data={this.state.data}
         ></Main>
         <HornedBeastModal
           showModal={this.state.showModal}
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data.json';
+
 import './Main.css';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="beasts">
-        {data.map((item, idx) => (
+        {this.props.data.map((item, idx) => (
           <HornedBeast
             title={item.title}
             imgUrl={item.image_url}
